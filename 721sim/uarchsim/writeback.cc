d96ca425116b3cdde9396b3b5072689c2285a636
Sirajus Salekin <ssalek14@earlham.edu>
2023-04-24 21:47:58 -0400

    turning off debug, leftover from retire logic experiments

--- 721sim/uarchsim/writeback.cc
+++ 721sim/uarchsim/writeback.cc
@@ -72,7 +72,7 @@ void pipeline_t::writeback(unsigned int lane_number) {
             // FIX_ME #15c END
 
             // Restore the LQ/SQ.
-            printf("before calling LSU.restore rec_SQ_index: %d, PAY.head.SQ_index: %d\n", PAY.buf[index].SQ_index, PAY.buf[PAY.head].SQ_index);
+            //printf("before calling LSU.restore rec_SQ_index: %d, PAY.head.SQ_index: %d\n", PAY.buf[index].SQ_index, PAY.buf[PAY.head].SQ_index);
             LSU.restore(PAY.buf[index].LQ_index, PAY.buf[index].LQ_phase, PAY.buf[index].SQ_index, PAY.buf[index].SQ_phase);
 
             // FIX_ME #15d
@@ -106,6 +106,7 @@ void pipeline_t::writeback(unsigned int lane_number) {
 
       // FIX_ME #16 BEGIN
       //printf("%X: index: %d writeback::set_complete(%d)\n",PAY.buf[index].pc, index, PAY.buf[index].checkpoint_ID);
+      //if (PAY.buf[index].checkpoint_ID == 3) printf("rt: %X\n", PAY.buf[index].pc);
       REN->set_complete(PAY.buf[index].checkpoint_ID);
       // FIX_ME #16 END
 
